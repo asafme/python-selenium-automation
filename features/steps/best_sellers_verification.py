dc54c084c3cc89275954791504652d211c7f188e
asafme <jessicaburgh@gmail.com>
2023-03-12 09:45:08 -0400

    ignore

--- features/steps/best_sellers_verification.py
+++ features/steps/best_sellers_verification.py
@@ -4,7 +4,25 @@ from selenium.webdriver.support import expected_conditions as EC
 from behave import when, given, then
 from time import sleep
 
-@given('Open Amazon Best Sellers Page')
+TOP_LINKS = (By.CSS_SELECTOR, "#zg_header ul li")
+HEADER = (By.CSS_SELECTOR, "#zg_banner_text")
+
+@given('Open Amazon Best Sellers')
 def open_amazon(context):
     context.driver.get('https://www.amazon.com/gp/bestsellers/?ref_=nav_cs_bestsellers')
 
+
+@then('User can click through top links and verify correct page opens')
+def click_through_top(context):
+    top_links = context.driver.find_elements(*TOP_LINKS)
+
+    for i in range(len(top_links)):
+        link_to_click = context.driver.find_elements(*TOP_LINKS)[i]
+        link_text = link_to_click.text
+        print(link_text)
+
+        link_to_click.click()
+
+        header_text = context.driver.find_element(*HEADER).text
+        print(header_text)
+        assert link_text in header_text, f'Expected {link_text} to be in {header_text}'
--- features/tests/Best Sellers Verification.feature
+++ features/tests/Best Sellers Verification.feature
@@ -3,5 +3,5 @@ Feature: Verify that Best Sellers links work
 
 
   Scenario: User can access each Best Sellers Link
-    Given Open Amazon Best Sellers Page
-    Then Verify each link opens correct page
+    Given Open Amazon Best Sellers
+    Then User can click through top links and verify correct page opens
